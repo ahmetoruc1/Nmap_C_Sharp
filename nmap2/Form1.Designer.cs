3f832fc979d31b0da2428f2d9f5cf3bdab46574d
ahmetoruc1 <ahmet-oruc2001@hotmail.com>
2022-06-24 09:22:51 +0300

    NMap

---
+++ nmap2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace nmap2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxHOST = new System.Windows.Forms.TextBox();
+            this.buttonoku = new System.Windows.Forms.Button();
+            this.buttonDUR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(80, 37);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 22);
+            this.textBoxIP.TabIndex = 0;
+            // 
+            // textBoxHOST
+            // 
+            this.textBoxHOST.Location = new System.Drawing.Point(52, 92);
+            this.textBoxHOST.Multiline = true;
+            this.textBoxHOST.Name = "textBoxHOST";
+            this.textBoxHOST.Size = new System.Drawing.Size(378, 197);
+            this.textBoxHOST.TabIndex = 0;
+            // 
+            // buttonoku
+            // 
+            this.buttonoku.Location = new System.Drawing.Point(196, 31);
+            this.buttonoku.Name = "buttonoku";
+            this.buttonoku.Size = new System.Drawing.Size(93, 35);
+            this.buttonoku.TabIndex = 1;
+            this.buttonoku.Text = "OKU";
+            this.buttonoku.UseVisualStyleBackColor = true;
+            this.buttonoku.Click += new System.EventHandler(this.buttonoku_Click);
+            // 
+            // buttonDUR
+            // 
+            this.buttonDUR.Location = new System.Drawing.Point(295, 31);
+            this.buttonDUR.Name = "buttonDUR";
+            this.buttonDUR.Size = new System.Drawing.Size(91, 35);
+            this.buttonDUR.TabIndex = 1;
+            this.buttonDUR.Text = "DUR";
+            this.buttonDUR.UseVisualStyleBackColor = true;
+            this.buttonDUR.Click += new System.EventHandler(this.buttonDUR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 350);
+            this.Controls.Add(this.buttonDUR);
+            this.Controls.Add(this.buttonoku);
+            this.Controls.Add(this.textBoxHOST);
+            this.Controls.Add(this.textBoxIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxHOST;
+        private System.Windows.Forms.Button buttonoku;
+        private System.Windows.Forms.Button buttonDUR;
+    }
+}
+
---
+++ nmap2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace nmap2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+        Thread thread = null;
+        //Thread in çalışması için Threading kütüphanesini projeye import ediyoruz.
+        
+
+        public void scan(string subnet)
+        {
+            Ping ping;
+            PingReply reply;
+            
+            for (int i = 1; i < 255; i++)
+            {
+                
+                string subnetn = "." + i.ToString();
+                ping = new Ping();
+                reply = ping.Send(subnet+subnetn, 100);
+
+                if (reply.Status==IPStatus.Success)
+                {
+                    try
+                    {
+                        textBoxHOST.AppendText(subnet + subnetn +Environment.NewLine);
+                    }
+                    catch (Exception hata)
+                    {
+                        MessageBox.Show(hata.Message,"Threadde Hata!");
+                    }
+                }
+            }
+        }
+
+        private void buttonoku_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(()=>scan(textBoxIP.Text));
+            thread.Start();
+
+            if (thread.IsAlive)
+            {
+                buttonoku.Enabled = false;
+                buttonDUR.Enabled = true;
+                textBoxIP.Enabled = false;
+            }
+        }
+        private void buttonDUR_Click(object sender, EventArgs e)
+        {
+            thread.Suspend();
+            buttonDUR.Enabled = false;
+            buttonoku.Enabled = true;
+            textBoxIP.Enabled = true;
+        }
+    }
+}
